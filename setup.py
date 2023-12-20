a096a2d0edb1a4651162706273757b9f25d1e125
Hardik Dava <39372750+hardikdava@users.noreply.github.com>
2023-12-20 18:17:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ import subprocess
 try:
     import torch
 except:
-    subprocess.run(["pip", "install", "torch"])
+    subprocess.run(["pip3", "install", "torch"])
 
 from setuptools import find_packages, setup
 from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
